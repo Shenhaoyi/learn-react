0f695e282de7ddeeb02d3de85cd6c840b3bd3e71
shenhaoyi <shenhaoyi04@163.com>
2023-08-31 15:05:16 +0800

    添加脚手架的注释

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 帮忙检查代码中不太合理的地方
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
+
+// 记录页面性能
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
