1632fa80afa264781353a89d46a45264a5cad29c
shenhaoyi <shenhaoyi04@163.com>
2023-09-03 02:00:32 +0800

    通过params给路由组件传参

--- src/pages/NestedRoutes/Route1/index.jsx
+++ src/pages/NestedRoutes/Route1/index.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Route1 extends Component {
   render() {
-    return <div>Route1</div>;
+    console.log('shen log: ', { props: this.props });
+    const { number, name } = this.props.match.params;
+    return <div>{`${name}，你接收到的数字是：${number}`},</div>;
   }
 }
--- src/pages/NestedRoutes/index.jsx
+++ src/pages/NestedRoutes/index.jsx
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import MyNavLink from '../../components/MyNavLink';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Route1 from './Route1';
 import Route2 from './Route2';
+
+const number = Math.random();
+const name = 'shen'
 
 export default class NestedRoutes extends Component {
   render() {
@@ -10,14 +13,14 @@ export default class NestedRoutes extends Component {
       <>
         <div>二级路由：</div>
         <div>
-          <MyNavLink to="/nested-routes/route1">route1</MyNavLink>
+          <MyNavLink to={`/nested-routes/route1/${number}/${name}`}>route1(params传参)</MyNavLink>
           &nbsp;
           <MyNavLink to="/nested-routes/route2">route2</MyNavLink>
         </div>
         <br />
         <Switch>
-          <Route path="/nested-routes/route1" component={Route1} />
-          <Route path="/nested-routes/route2" component={Route2} />
+          <Route path="/nested-routes/route1/:number/:name" component={Route1} />
+          <Route path="/nested-routes/route2/" component={Route2} />
           <Redirect to="/nested-routes/route1" />
         </Switch>
       </>
