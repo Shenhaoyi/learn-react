13936a0e068247c6e3330f5ce7045480c3b434ae
shenhaoyi <shenhaoyi04@163.com>
2023-09-02 14:10:33 +0800

    feat:替换为路由切换

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { Component } from 'react';
 import Hello from './components/Hello';
 import TestAxios from './components/TestAxios';
 import GithubSearch from './components/GithubSearch';
+import { Link, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <Hello></Hello>
-        <br></br>
-        <TestAxios></TestAxios>
-        <br></br>
-        <br></br>
-        <GithubSearch></GithubSearch>
+        <div>
+          {/* 原生html，切换页面的方式：<a href="/aa">aa</a> */}
+          <Link to="/about">Hello</Link>&nbsp;
+          <Link to="/axios">测试axios</Link>&nbsp;
+          <Link to="/github-search">Hello</Link>
+        </div>
+        <br />
+        <Route path="/about" component={Hello} />
+
+        <Route path="/axios" component={TestAxios} />
+
+        <Route path="/github-search" component={GithubSearch} />
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // 帮忙检查代码中不太合理的地方
   <React.StrictMode>
-    <App />
+    {/* 整个应用都要用一个router来管理 */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
-
 
 // 记录页面性能
 // If you want to start measuring performance in your app, pass a function
