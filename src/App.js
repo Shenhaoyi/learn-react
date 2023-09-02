6d006692a01f4a87456642a93856bfc6cb2a0f59
shenhaoyi <shenhaoyi04@163.com>
2023-09-03 00:44:57 +0800

    feat: 使用Redirect组件进行兜底

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import Hello from './pages/Hello';
 import TestAxios from './pages/TestAxios';
 import GithubSearch from './pages/GithubSearch';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import MyNavLink from './components/MyNavLink';
 
 export default class App extends Component {
@@ -11,7 +11,7 @@ export default class App extends Component {
       <>
         <div>
           {/* 原生html，切换页面的方式：<a href="/aa">aa</a> */}
-          <MyNavLink to="/about">Hello</MyNavLink>
+          <MyNavLink to="/hello">Hello</MyNavLink>
           &nbsp;
           <MyNavLink to="/axios">测试axios</MyNavLink>
           &nbsp;
@@ -19,9 +19,11 @@ export default class App extends Component {
         </div>
         <br />
         <Switch>
-          <Route path="/about" component={Hello} />
+          <Route path="/hello" component={Hello} />
           <Route path="/axios" component={TestAxios} />
           <Route path="/github-search" component={GithubSearch} />
+          {/* 如果当前的路径到Redirect还没有匹配到，就重定向（后面如果再写Route就不会生效了） */}
+          <Redirect to="/hello" />
         </Switch>
       </>
     );
