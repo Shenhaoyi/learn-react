cd97562fdf3841a5710407c2426f9c1750bc0384
shenhaoyi <shenhaoyi04@163.com>
2023-09-03 14:35:12 +0800

    使用api跳转路由

--- src/pages/NestedRoutes/index.jsx
+++ src/pages/NestedRoutes/index.jsx
@@ -4,11 +4,24 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Route1 from './Route1';
 import Route2 from './Route2';
 import Route3 from './Route3';
+import { Button } from 'antd';
 
 const number = Math.random();
 const name = 'shen';
 
 export default class NestedRoutes extends Component {
+  routeChange = (params) => {
+    return () => {
+      const { replace = true, path, state = null } = params;
+      console.log('shen log: ', { state });
+      const { history } = this.props;
+      if (replace) {
+        history.replace(path, state);
+      } else {
+        history.push(path, state);
+      }
+    };
+  };
   render() {
     return (
       <>
@@ -35,6 +48,36 @@ export default class NestedRoutes extends Component {
           >
             route3(state传参)
           </MyNavLink>
+        </div>
+        <div>
+          编程式路由导航：
+          <Button
+            onClick={this.routeChange({
+              path: `/nested-routes/route1/${number}/${name}`,
+            })}
+          >
+            route1(params传参，replace)
+          </Button>
+          &nbsp;
+          <Button
+            onClick={this.routeChange({
+              path: `/nested-routes/route2/?number=${number}&name=${name}`,
+            })}
+          >
+            route2(search传参，replace)
+          </Button>
+          &nbsp;
+          <Button
+            onClick={this.routeChange({
+              path: '/nested-routes/route3',
+              state: {
+                number,
+                name,
+              },
+            })}
+          >
+            route3(state传参，replace，哈希路由时不支持！)
+          </Button>
         </div>
         <br />
         <Switch>
