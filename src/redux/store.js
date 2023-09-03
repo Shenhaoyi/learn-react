129d17bd76e3c64942c7c7a4c90e7ad68a758125
shenhaoyi <shenhaoyi04@163.com>
2023-09-04 03:39:15 +0800

    安装redux-devtools-extension以支持浏览器插件

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,11 @@
   对外暴露一个store对象，整个应用只有一个store对象。
  */
 
-import { legacy_createStore as createStore } from 'redux'; // createStore提示被弃用了
+import { legacy_createStore as createStore } from 'redux'; // createStore被弃用了
 import countReducer from './countReducer';
 
 
-export default createStore(countReducer);
+export default createStore(
+  countReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
