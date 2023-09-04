38d25974b33e7c4b8a7dcf7c40f3872ac4e266ea
shenhaoyi <shenhaoyi04@163.com>
2023-09-04 16:52:47 +0800

    组件懒加载

--- src/pages/Extension/index.jsx
+++ src/pages/Extension/index.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import SetState from './SetState';
 import MyNavLink from '../../components/MyNavLink';
+import { Spin } from 'antd';
+const LazyLoad = lazy(() => import('./LazyLoad'));
 
 export default class NestedRoutes extends Component {
   render() {
@@ -12,12 +14,17 @@ export default class NestedRoutes extends Component {
         <div>
           <MyNavLink to="/extension/set-state">setState</MyNavLink>
           &nbsp;
+          <MyNavLink to="/extension/lazy-load">setState</MyNavLink>
+          &nbsp;
         </div>
         <br />
-        <Switch>
-          <Route path="/extension/set-state" component={SetState} />
-          <Redirect to="/extension/set-state" />
-        </Switch>
+        <Suspense fallback={<Spin />}>
+          <Switch>
+            <Route path="/extension/set-state" component={SetState} />
+            <Route path="/extension/lazy-load" component={LazyLoad} />
+            <Redirect to="/extension/set-state" />
+          </Switch>
+        </Suspense>
       </>
     );
   }
