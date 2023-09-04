a7b3074b69365f36642dcde39a0f11a51b37a387
shenhaoyi <shenhaoyi04@163.com>
2023-09-04 14:53:32 +0800

    setState进阶用法

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import MyNavLink from './components/MyNavLink';
 import NestedRoutes from './pages/NestedRoutes';
 import TestRedux from './pages/TestRedux';
+import Extension from './pages/Extension';
 
 export default class App extends Component {
   render() {
@@ -22,6 +23,8 @@ export default class App extends Component {
           <MyNavLink to="/nested-routes">嵌套路由</MyNavLink>
           &nbsp;
           <MyNavLink to="/redux">测试redux</MyNavLink>
+          &nbsp;
+          <MyNavLink to="/extension">拓展内容</MyNavLink>
         </div>
         <br />
         <Switch>
@@ -30,6 +33,7 @@ export default class App extends Component {
           <Route path="/github-search" component={GithubSearch} />
           <Route path="/nested-routes" component={NestedRoutes} />
           <Route path="/redux" component={TestRedux} />
+          <Route path="/extension" component={Extension} />
           {/* 如果当前的路径到Redirect还没有匹配到，就重定向（后面如果再写Route就不会生效了） */}
           <Redirect to="/hello" />
         </Switch>
