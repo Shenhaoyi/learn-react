1a560f695b4ba9a94f02f1616c0ae71d3573cc6c
shenhaoyi <shenhaoyi04@163.com>
2023-09-09 15:15:34 +0800

    useEffect模拟生命周期

--- src/pages/Extension/UseHooks/index.jsx
+++ src/pages/Extension/UseHooks/index.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function UseHooks() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('shen log: ', '组件渲染就执行');
+  });
+
+  useEffect(() => {
+    console.log('shen log: ', '组件第一次挂载就执行');
+    const timer = setInterval(() => {
+      setCount((oldValue) => oldValue + 1); // 使用函数
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+      console.log('shen log: ', '组件卸载啦');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('shen log: ', '初始化或者count更新啦', { count });
+  }, [count]);
+
   const add = () => {
     // setCount(count + 1); // 直接传值
     setCount((oldValue) => oldValue + 1); // 使用函数
   };
+
   return (
-    <>
+    <div>
       <div>count: {count}</div>
       <button onClick={add}>点我+1</button>
-    </>
+    </div>
   );
 }
